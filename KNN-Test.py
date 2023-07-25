4b334b5e7ead7d83363c81e75020d6381f2313ab
SanahR <136401567+SanahR@users.noreply.github.com>
2023-07-24 19:54:48 -0500

    Adding a docstring
    Adding a long docstring with multiple sections, explanations, definitions, and a couple resources.

--- KNN-Test.py
+++ KNN-Test.py
@@ -1,3 +1,35 @@
+"""
+A short program exploring KNearestNeighbors using simple data. 
+If you want to know about the KNearestNeighbors algorithm, here is an article, https://en.wikipedia.org/wiki/K-nearest_neighbors_algorithm
+The plot here has no relation to the algorithm and was made for learning purposes. 
+The masking system also has little relation to the algorithm and is also just for the plot. 
+Definitions(in this specific project):
+Plot: A graph made with matplotlib.
+KNC: KNeighborsClassifier, or KNearestNeighbors.
+X = the data for the heights.
+y = part of the training data, it shows which heights are 5th grade and 6th grade. 
+About KNearestNeighbors: 
+KNearestNeighbors is an algorithm that classifies  numbers into categories based on data you've already given it. 
+The way this works is by searching for the closest NEIGHBOR to the 
+number inputted by the user. Using the data you have given it, it will classify what it is given. 
+About the Plot: 
+This plot is a simple line plot with two lines, one representing fifth grade and one representing 6th grade. 
+The markers and colors have little meaning here, other than differentiation. The masking system here was used simply to separate the two grades and not have to do it 
+manually, although one could argue that it could've been done in a more efficient manner. 
+Step-by-Step: 
+Step 1: Making the data
+- This step is just making the heights and using y to classify them. 
+Step 2: Creating the algorithm
+- This is just one line of code where we create the algorithm(or KNC)
+Step 3: Learning/Training
+- This is where we fit X to y, and the algorithm makes the connections that they line up. For example, 
+this stage is where the algorithm makes the connection that the 4.8 kid is a 5th grader. 
+Step 4: Prediction/Testing
+This is where the program uses what it knows to determine the nearest neighbor in the data to classify the kid. 
+Step 5: Plotting
+This is just a standard plot showcasing the data. 
+"""
+#adding the imports
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier as KNC
 import matplotlib.pyplot as plt
@@ -32,6 +64,7 @@ algo.fit(X,y)
 
 # Step 4: Prediction/Testing
 #Adds an interactive element
+#Floats are used in place of integers in order to make more precise heights. 
 ask = float(input("What is the student's height? ")) 
 print(algo.predict([[ask]]))
 #Two brackets needed in order to pass as 2D
